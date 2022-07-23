514dd274b4ce34229cf6163bc2824c19c1cfc236
Eric Walker <eric.walker@gmail.com>
2022-07-22 23:31:41 -0600

    Placeholder code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# geotime
-128-bit UNIX timestamps
+# Geotime
+
+Geotime provides a 128-bit signed integer timestamp compatible with Unix `time_t` and anchored at the [Unix epoch](https://en.wikipedia.org/wiki/Unix_time).
+
+A 128-bit timestamp allows one to represent times of events in geological, historical and present-day time to millisecond precision.  We go down to milliseconds as a convenience for handling timestamps for recent events.  In order to maintain a clean mapping to Unix timestamps, we inherit whatever is going on with leap seconds.
---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+#[macro_use]
+extern crate quick_error;
+
+use chrono::{DateTime, Utc};
+
+quick_error! {
+    #[derive(Clone, Debug)]
+    pub enum Error {
+        TryFromInt(err: std::num::TryFromIntError) {
+            from()
+        }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Geotime(i128);
+
+impl From<i64> for Geotime {
+    fn from(n: i64) -> Self {
+        Self(n.into())
+    }
+}
+
+impl From<&DateTime<Utc>> for Geotime {
+    fn from(dt: &DateTime<Utc>) -> Self {
+        Self::from(dt.timestamp_millis())
+    }
+}
+
+impl Geotime {
+    pub fn timestamp_millis(&self) -> Result<i64> {
+        Ok(self.0.try_into()?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod chrono {
+        use super::*;
+        use ::chrono::{TimeZone, Utc};
+
+        #[test]
+        fn from_chrono() {
+            let ut = Utc.ymd(1800, 1, 1).and_hms_milli(0, 0, 0, 0);
+            let gt = Geotime::from(&ut);
+            assert_eq!(ut.timestamp_millis(), gt.timestamp_millis().unwrap());
+        }
+    }
+}
